06567d012a97415befbe827077647c6416242462
Rogelio Gonález López <68076885+rogerGZLL@users.noreply.github.com>
2022-01-07 12:53:00 -0600

    main

--- lib/main.dart
+++ lib/main.dart
@@ -10,30 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return const GetMaterialApp(
-<<<<<<< HEAD
-<<<<<<< HEAD
-        title: 'Assurance Manager prueba',
-=======
-        title: 'Assurance Manager_',
->>>>>>> bacf0126f3f8ca278b2cd52cb35f717f2611e60a
-=======
-        title: 'Assurance Manager prueba',
->>>>>>> Roger
-        smartManagement: SmartManagement.keepFactory,
-        debugShowCheckedModeBanner: false,
-        localizationsDelegates: [
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        supportedLocales: [
-          Locale('en'),
-          Locale('es'),
-        ],
-        home: SplashPage());
-=======
     return GestureDetector(
       onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
       child: GetMaterialApp(
@@ -55,6 +31,5 @@ class MyApp extends StatelessWidget {
           ],
           home: const SplashPage()),
     );
->>>>>>> Roger
   }
 }
