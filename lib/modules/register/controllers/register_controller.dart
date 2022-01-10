d88e32fa5e92384ea09f2e3d393e9684c2a3d045
Rogelio Gonález López <68076885+rogerGZLL@users.noreply.github.com>
2022-01-10 12:46:19 -0600

    2021-01-10

--- lib/modules/register/controllers/register_controller.dart
+++ lib/modules/register/controllers/register_controller.dart
@@ -1,8 +1,12 @@
+import 'package:assurance/Firebase/firebase_errors.dart';
+import 'package:assurance/Firebase/firebase_services.dart';
 import 'package:assurance/constants/strings.dart';
+import 'package:assurance/modules/home/pages/home_page.dart';
 import 'package:assurance/utils/utils.dart';
 import 'package:assurance/utils/utils_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:progress_dialog/progress_dialog.dart';
 
 class RegisterController extends GetxController {
   final TextEditingController _tecNombre = TextEditingController();
@@ -17,14 +21,68 @@ class RegisterController extends GetxController {
   TextEditingController get tecPasswordConfirm => _tecPasswordConfirm;
   bool _termsAndConditions = false;
   bool get termsAndConditions => _termsAndConditions;
+  ProgressDialog _progressDialog;
 
   @override
   void onReady() {
     super.onReady();
+    _progressDialog = UtilsDialog.showProgresDialog(Get.overlayContext, false);
+    llenarDatos();
+  }
+
+  void llenarDatos() {
+    _tecNombre.text = 'Rogelio';
+    _tecEmail.text = 'rogegzll96@gmail.com';
+    _tecTelefono.text = '3314653839';
+    _tecPassword.text = 'welcome';
+    _tecPasswordConfirm.text = 'welcome';
   }
 
   void registrarUsuario() {
-    if (validarRegistro()) {}
+    if (validarRegistro()) {
+      registrarUsuarioFirebase();
+    }
+  }
+
+  void registrarUsuarioFirebase() {
+    _progressDialog.show();
+    FirebaseServices.firebaseAuth
+        .createUserWithEmailAndPassword(
+            email: _tecEmail.text.trim(), password: _tecPassword.text)
+        .then((res) {
+      crearUsuarioFirebase(res.user.uid);
+    }).catchError((onError) {
+      Future.delayed(Duration(milliseconds: 300)).then((value) {
+        _progressDialog.hide().whenComplete(() {
+          UtilsDialog.alertDialogError(Get.overlayContext,
+              FirebaseErrors.erroresFirebase(onError.toString()));
+        });
+      });
+    });
+  }
+
+  void crearUsuarioFirebase(String uid) {
+    Map data = {
+      'nombre': _tecNombre.text,
+      'correo': _tecEmail.text,
+      'telefono': _tecTelefono.text,
+      'avatar': '',
+      'rfc': '',
+      'correoNewYear': false,
+      'correoBirthday': false,
+      'correoChristmas': false,
+      'correoRecordatorioPagos': false,
+      'fechaCreado': DateTime.now().millisecondsSinceEpoch,
+      'fechaModificado': DateTime.now().millisecondsSinceEpoch,
+      'borrado': false
+    };
+    FirebaseServices.databaseReference
+        .child('agentesSeguro')
+        .child(uid)
+        .set(data)
+        .then((value) {
+      toHomePage();
+    }).catchError((onError) {});
   }
 
   bool validarRegistro() {
@@ -75,10 +133,16 @@ class RegisterController extends GetxController {
     return valido;
   }
 
-  void registrarUsuarioFirebase() {}
-
   void setTermsAndConditions(bool value) {
     _termsAndConditions = value;
     update();
+  }
+
+  void toHomePage() {
+    Future.delayed(const Duration(milliseconds: 200)).then((value) {
+      _progressDialog.hide().whenComplete(() {
+        Get.offAll(() => HomePage());
+      });
+    });
   }
 }
